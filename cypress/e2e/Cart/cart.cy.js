587589c02e5ac01d8b5fdecdcb2a9208930d3af0
molambat <lambodripgc@gmail.com>
2025-04-10 20:45:22 +0200

    Cart

--- cypress/e2e/Cart/cart.cy.js
+++ cypress/e2e/Cart/cart.cy.js
@@ -23,15 +23,16 @@ describe('Cart Functionality - LamboDrip', () => {
   });
 
   it('should allow changing the quantity of a product in the cart', () => {
-    cy.get('a[href*="/products"]').filter(':visible').first().click({ force: true });
-    cy.wait(400);
+    cy.get('a[href*="/products/"]').filter(':visible').first().click({ force: true });
     cy.get('button[name="add"]').should('exist').click({ force: true });
     cy.wait(400);
     cy.visit(`${Cypress.config().baseUrl}/cart`);
-    cy.get('input[id="Quantity-1"]').clear().type('2');
-    cy.focused().trigger('keydown', { keyCode: 9, which: 9 });
+    
+    cy.get('.cart-items', { timeout: 10000 }).should('exist');
+    cy.get('input.quantity__input', { timeout: 10000 }).first().clear().type('2');
+    cy.focused().trigger('keydown', { keyCode: 9, which: 9 }); // tab pour "trigger" une maj
     cy.wait(400);
-    cy.get('input[id="Quantity-1"]').should('have.value', '2');
+    cy.get('input.quantity__input').first().should('have.value', '2');
   });
 
   it('should allow removing a product from the cart', () => {
